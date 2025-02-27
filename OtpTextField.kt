4e1816fca5d5edaa2ce7f3e1eae4bfce32e35152
Umair Abdullah <113193169+rajaumair7890@users.noreply.github.com>
2025-02-27 20:29:41 +0500

    Update OtpTextField.kt

--- OtpTextField.kt
+++ OtpTextField.kt
@@ -1,3 +1,48 @@
+package com.codingWithUmair.otpTextField
+
+import androidx.annotation.IntRange
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.selection.LocalTextSelectionColors
+import androidx.compose.foundation.text.selection.TextSelectionColors
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.surfaceColorAtElevation
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
 @Composable
 fun OtpTextField(
 	value: String,
@@ -8,6 +53,10 @@ fun OtpTextField(
 	accentColor: Color = MaterialTheme.colorScheme.primary,
 	textColor: Color = MaterialTheme.colorScheme.onSurface,
 	style: OtpTextFieldStyle = OtpTextFieldStyle.BORDERED_BOX,
+	keyboardOptions: KeyboardOptions = KeyboardOptions(
+		keyboardType = KeyboardType.NumberPassword
+	),
+	keyboardActions: KeyboardActions = KeyboardActions.Default
 ) {
 	val customTextSelectionColors = remember {
 		TextSelectionColors(
@@ -35,6 +84,8 @@ fun OtpTextField(
 			},
 			textStyle = TextStyle(color = Color.Transparent),
 			cursorBrush = SolidColor(Color.Unspecified),
+			keyboardOptions = keyboardOptions,
+			keyboardActions = keyboardActions,
 			modifier = modifier
 		)
 	}
@@ -55,7 +106,7 @@ private fun OtpTextFieldDecorationBox(
 			verticalAlignment = Alignment.CenterVertically,
 			horizontalArrangement = Arrangement.spacedBy(12.dp, Alignment.CenterHorizontally)
 		){
-			(0..maxOtpDigits).forEach { index ->
+			(0..< maxOtpDigits).forEach { index ->
 				OtpTextFieldItem(
 					value = (value.getOrNull(index) ?: "").toString(),
 					style = style,
